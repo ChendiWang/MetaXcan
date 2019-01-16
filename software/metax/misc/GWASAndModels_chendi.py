d0045d40f08e6a980538d487bec6f0c8d799da36
Chendi Wang <chendi.wang.judy@gmail.com>
2019-01-16 12:30:32 -0800

    Change logging levels for trivial info

--- software/metax/misc/GWASAndModels_chendi.py
+++ software/metax/misc/GWASAndModels_chendi.py
@@ -9,7 +9,7 @@ from ..gwas import Utilities as GWASUtilities
 
 """
 This function: strand_switch, convert the allele to the paried base
-Edited by Chendi on 2019-01-01
+Edited by Chendi on 2019-01-11
 """
 def strand_switch(input_data):  ## TODO: Find a more effecient way to do this
     output=[]
@@ -33,7 +33,7 @@ def strand_switch(input_data):  ## TODO: Find a more effecient way to do this
 
 """
 This function: align_data_to_alleles, handles 5 cases for allele coding scheme correction
-Edited by Chendi on 2019-01-01
+Edited by Chendi on 2019-01-11
 """
 def align_data_to_alleles(data, base, left_on, right_on):
     EA, NEA = Constants.EFFECT_ALLELE, Constants.NON_EFFECT_ALLELE
@@ -52,28 +52,28 @@ def align_data_to_alleles(data, base, left_on, right_on):
     merged = merged[eq]
     if eq.shape[0] == 0:
         return merged # Nothing 
-    logging.info("%s rows of merged data after deleting strand switch", merged.shape[0])
+    logging.log(9, "%s rows of merged data after deleting strand switch", merged.shape[0])
     """
 
     # This set information can be used for both strand switch correction, and discarding (ambiguous case + seem same case)
     alleles_1_2 = pandas.Series([set(e) for e in zip(merged[EA_BASE], merged[NEA_BASE], merged[EA], merged[NEA])])
-    # logging.info("%s rows in alleles_1_2", alleles_1_2.shape[0])
-    # logging.info("alleles1_2 ENtry 0: %s", alleles_1_2[0])
-    # logging.info("alleles1_2 Entry 1: %s", alleles_1_2[1])
+    # logging.log(9, "%s rows in alleles_1_2", alleles_1_2.shape[0])
+    # logging.log(9, "alleles1_2 ENtry 0: %s", alleles_1_2[0])
+    # logging.log(9, "alleles1_2 Entry 1: %s", alleles_1_2[1])
 
     # =============== Handle strand switch ===============
     switched = alleles_1_2 == set([u'A', u'T', u'C', u'G'])  
     # switched[0] = True # Toggle this line to TEST: single case strand switch
     # switched = alleles_1_2 == set([u'A', u'G']) # Toggle this line to TEST: multiple strand switch
-    logging.info("%s rows to be switched", sum(switched))
+    logging.log(9, "%s rows to be switched", sum(switched))
 
-    logging.info("EA Entry to be switched: %s", merged.loc[switched, EA]) ## TODO: change into --verbosity 1 level output
+    logging.log(9, "EA Entry to be switched: %s", merged.loc[switched, EA])
     merged.loc[switched, EA] = strand_switch(merged.loc[switched, EA])
-    logging.info("EA Entry after being switched: %s", merged.loc[switched, EA])
+    logging.log(9, "EA Entry after being switched: %s", merged.loc[switched, EA])
 
-    logging.info("NEA Entry to be switched: %s", merged.loc[switched, NEA])
+    logging.log(9, "NEA Entry to be switched: %s", merged.loc[switched, NEA])
     merged.loc[switched, NEA] = strand_switch(merged.loc[switched, NEA])
-    logging.info("NEA Entry after being switched: %s", merged.loc[switched, NEA])
+    logging.log(9, "NEA Entry after being switched: %s", merged.loc[switched, NEA])
      
     # =============== Handle ambiguous cases (switch | flip) & seems exact cases (but with switch & flip) ===============   
     # This has to be behind the strand switch correction: avoid index errors also, if A T vs. T A etc. cases happen after strand correction, don't have to discard
@@ -84,23 +84,23 @@ def align_data_to_alleles(data, base, left_on, right_on):
 
     """ ### if want to save detailed information about which cases
     delete_case_AT = alleles_1_2 == set([u'A', u'T'])
-    logging.info("%s rows to be deleted due to A&T case", sum(delete_case_AT))
+    logging.log(9, "%s rows to be deleted due to A&T case", sum(delete_case_AT))
     delete_case_CG = alleles_1_2 == set([u'C', u'G'])
-    logging.info("%s rows to be deleted due to C&G case", sum(delete_case_CG)) """
+    logging.log(9, "%s rows to be deleted due to C&G case", sum(delete_case_CG)) """
 
     keep_case = (alleles_1_2 != set([u'A', u'T'])) & (alleles_1_2 != set([u'C', u'G'])) 
     # keep_case[0] = False # Toggle this line to TEST: drop lines when there is no cases to be deleted
-    logging.info("%s rows to be DELETED", sum(keep_case==False))
-    logging.info("%s rows to be KEPT", sum(keep_case))
+    logging.log(9, "%s rows to be DELETED", sum(keep_case==False))
+    logging.log(9, "%s rows to be KEPT", sum(keep_case))
 
     merged = merged[keep_case]
-    logging.info("%s rows of merged data after dropping the discarded cases", merged.shape[0])   
+    logging.log(9, "%s rows of merged data after dropping the discarded cases", merged.shape[0])   
 
     # =============== Handle allele flip =============== 
     flipped = merged[EA] != merged[EA_BASE]
 
-    logging.info("%s rows to be flipped", sum(flipped))
-    # logging.info(flipped)
+    logging.log(9, "%s rows to be flipped", sum(flipped))
+    # logging.log(9, "%s Bases to be flipped", flipped)
 
     Z = Constants.ZSCORE
     if Z in merged:
@@ -114,9 +114,9 @@ def align_data_to_alleles(data, base, left_on, right_on):
 
     # =============== Handle random error: eg. AC vs. AT =============== 
     keep_case_final = (merged[EA] == merged[EA_BASE]) & (merged[NEA] == merged[NEA_BASE])
-    logging.info("%s rows before final checking", merged.shape[0])
+    logging.log(9, "%s rows before final checking", merged.shape[0])
     merged = merged[keep_case_final]
-    logging.info("%s rows after final checking", merged.shape[0])
+    logging.log(9, "%s rows after final checking", merged.shape[0])
 
     return merged
 
